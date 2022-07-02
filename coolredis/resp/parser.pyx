be00453b21a9ab6c4858bc3059fc74fd2947983d
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2022-07-03 00:09:21 +0100

    fix rparser

--- coolredis/resp/parser.pyx
+++ coolredis/resp/parser.pyx
@@ -1,15 +1,15 @@
-from libc.stdlib import (
+from libc.stdlib cimport (
     strtoll,
 )
 
 cdef:
     char* LINE_TERMINATOR = "\r\n"
 
-    char* STRING_PREFIX = "+"
-    char* ERROR_PREFIX = "-"
-    char* INTEGER_PREFIX = ":"
-    char* BULK_STRING_PREFIX = "$"
-    char* ARRAY_PREFIX = "*"
+    char STRING_PREFIX = "+"
+    char ERROR_PREFIX = "-"
+    char INTEGER_PREFIX = ":"
+    char BULK_STRING_PREFIX = "$"
+    char ARRAY_PREFIX = "*"
 
     # Parsers for individual types, assuming the prefix and NL char has already been removed.
     char* parse_string(char* line):
@@ -19,9 +19,18 @@ cdef:
         return parse_string(line)
     
     long long int parse_int(char* line):
-        return strtoll(line)
+        return strtoll(line, NULL, 0)
+    
+    enum DataType:
+        STRING = 0
+        ERROR = 1
+        INT = 2
+        BULK_STRING = 3
+        ARRAY = 4
 
-cpdef parse_type(bytes redis_line):
+cpdef struct ErrorType
+
+cdef parse_type(bytes redis_line):
     cdef char* line = <char*> redis_line
     cdef first_byte = line[0]
 
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 from coolredis.resp import parser
 
-print(parser.parse_type(":727"))
+print(parser.parse_type(b":727"))
